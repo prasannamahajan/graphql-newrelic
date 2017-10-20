49670fc45794fc267c008cba07537b33c7ee947d
prasanna.mahajan <prasanna.mahajan@hotmail.com>
2017-10-20 13:16:47 +0530

    Removing unnecessary code

--- main.go
+++ main.go
@@ -2,13 +2,8 @@ package main
 
 import (
 	"encoding/json"
-	//"fmt"
-	"fmt"
 	"github.com/arvitaly/go-graphql-tools"
 	"github.com/arvitaly/graphql"
-	"github.com/graphql-go/graphql/language/ast"
-	"github.com/graphql-go/graphql/language/parser"
-	"github.com/graphql-go/graphql/language/source"
 	"graphql-newrelic/data"
 	"graphql-newrelic/relicconf"
 	"graphql-newrelic/router"
@@ -16,8 +11,6 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
-	"strings"
 )
 
 func InitSchema() graphql.Schema {
@@ -34,84 +27,36 @@ func InitSchema() graphql.Schema {
 }
 
 func executeQuery(query string, schema graphql.Schema) *graphql.Result {
-	fmt.Println("query is : ", query)
+	log.Println("query is : ", query)
 	result := graphql.Do(graphql.Params{
 		RequestString: query,
 		Schema:        schema,
 	},
 	)
 	if len(result.Errors) > 0 {
-		fmt.Println("some error occured")
+		log.Println("some error occured")
 	}
 	return result
 }
 
-func getTypesFromQuery(q string) (string, error) {
-	source := source.NewSource(&source.Source{
-		Body: []byte(q),
-		Name: "GraphQL request",
-	})
-	querytypes := make([]string, 0)
-	AST, err := parser.Parse(parser.ParseParams{Source: source})
-	if err != nil {
-		fmt.Println(err)
-		return "", err
-	}
-	for _, definition := range AST.Definitions {
-		switch definition := definition.(type) {
-		case *ast.OperationDefinition:
-			for _, selection := range definition.SelectionSet.Selections {
-				switch selection := selection.(type) {
-				case *ast.Field:
-					querytypes = append(querytypes, selection.Name.Value)
-				}
-			}
-		}
-	}
-	return strings.Join(querytypes, ","), nil
-}
-
-var gquery string
-
 func queryHandler(w http.ResponseWriter, r *http.Request) {
-	result := executeQuery(gquery, Schema)
+	body, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	query := string(body)
+	result := executeQuery(query, Schema)
 	json.NewEncoder(w).Encode(result)
-}
-
-func slog(h http.HandlerFunc) http.HandlerFunc {
-	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		body, _ := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
-		gquery = string(body)
-		types, _ := getTypesFromQuery(gquery)
-		fmt.Println("types :-", types)
-		app := relicconf.GetRelicApp()
-		txn := app.StartTransaction(types, nil, nil)
-		defer txn.End()
-		h.ServeHTTP(w, r) // call original
-	})
 }
 
 var Schema graphql.Schema
 
-func dummyQuery() {
-	q := `query {
-				user{
-					name
-				}
-			}`
-	res := executeQuery(q, Schema)
-	json.NewEncoder(os.Stdout).Encode(res)
-}
 func init() {
 	Schema = InitSchema()
 	relicconf.InitNewRelic()
 	data.ImportData()
-	//	dummyQuery()
 }
 
 func main() {
-	http.HandleFunc("/graphql", slog(queryHandler))
-	fmt.Println("Starting Server")
+	http.HandleFunc("/graphql", queryHandler)
+	log.Println("Starting Server")
 	http.ListenAndServe(":8080", nil)
 }
